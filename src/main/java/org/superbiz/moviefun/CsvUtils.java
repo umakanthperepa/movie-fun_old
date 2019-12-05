f0b83e33b38554f3f2a54c97c3a554cfce11cbdc
umakanthperepa <Venkat_Umakanth@dell.com>
2019-12-05 12:03:40 +0530

    Read as Stream changes

--- src/main/java/org/superbiz/moviefun/CsvUtils.java
+++ src/main/java/org/superbiz/moviefun/CsvUtils.java
@@ -3,28 +3,32 @@ package org.superbiz.moviefun;
 import com.fasterxml.jackson.databind.MappingIterator;
 import com.fasterxml.jackson.databind.ObjectReader;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class CsvUtils {
 
+
     public static String readFile(String path) {
+
         try {
-            Scanner scanner = new Scanner(new File(path)).useDelimiter("\\A");
+
+            InputStream is = CsvUtils.class.getClassLoader().getResourceAsStream(path);
+            Scanner scanner = new Scanner(is).useDelimiter("\\A");
+
 
             if (scanner.hasNext()) {
                 return scanner.next();
             } else {
                 return "";
             }
-
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+        } catch (Exception e) {
+            throw new RuntimeException("Error extracting file", e);
         }
+
     }
 
     public static <T> List<T> readFromCsv(ObjectReader objectReader, String path) {
@@ -36,7 +40,6 @@ public class CsvUtils {
             while (iterator.hasNext()) {
                 results.add(iterator.nextValue());
             }
-
             return results;
         } catch (IOException e) {
             throw new RuntimeException(e);
